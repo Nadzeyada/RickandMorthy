0e8b5876ea384e78b15c6029eb0ce1d911a592c8
Nadzeya Kaliada <nadzeya.kaliada@ventionteams.com>
2025-11-13 21:56:29 +0100

    feat: add form

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+// Categories switching
 let sourceCards = [];
 const navTitles = document.querySelectorAll('.navbar-link');
 const listItems = document.querySelectorAll('.portfolio-section__list-item');;
 const cardsContainer = document.querySelector('.portfolio-section__cards-container');
 const loader = document.querySelector('.loader');
 
+// Error popup
 const popupError = document.querySelector('.portfolio-section__popup');
 const popupMessage = document.querySelector('.popup__message');
 const popupClose = document.querySelector('.popup__close-button');
+
+const form = document.querySelector('.form');
 
 function showError(message) {
   popupMessage.textContent = `Error fetching data: ${message}`;
@@ -156,9 +160,51 @@ navTitles.forEach((title) => {
     console.log(event.target);
   })
 });
+
 // === Closing popupError ===
 popupClose.addEventListener('click', ()=>{
   popupError.classList.add('hidden')
 })
+
+// === Validating form & popup
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const data = Object.fromEntries(formData.entries());
+
+  let valid =true;
+  const errors = document.querySelectorAll('.form__error');
+  for(let error of errors) {
+    error.textContent="";
+  }
+
+  if(data.username === "") {
+    errors[0].textContent = "Please, enter your first name.";
+    valid = false;
+  }
+
+  if(data.email === "") {
+    errors[1].textContent = "Please, enter a valid email address.";
+    valid = false;
+  }
+
+  if(!data.consent) {
+    errors[2].textContent = "Please, consent to the processing of personal data.";
+    valid = false;
+  }
+
+  if(valid) {
+    document.querySelectorAll('.popup__message')[1].innerHTML=`Form submitted successfully!<br>${data.username}<br>${data.email}`;
+    document.querySelector('.form__popup').classList.remove('hidden');
+    form.reset();
+  }
+})
+
+// === Closing form submission confirmation ===
+document.querySelector('.form__popup .popup__close-button').addEventListener('click', ()=>{
+  document.querySelector('.form__popup').classList.add('hidden');
+})
+
 generateCards();
 
